2559fed6857f901814da58436ec43df892f6efd1
Sirajuddin Asjad <sirasjad@gmail.com>
2017-05-06 20:52:44 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+// 				  Configuration file
+// ==================================================
+
+$name = 'Diverse Roleplay';
+$version = '1.0';
+$url = 'http://sirageek.com';
+$email = '#';
+
+$developer = 'Sira';
+$devcontact = 'sirasjad@gmail.com';
+
+?>
