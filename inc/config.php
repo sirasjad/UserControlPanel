df39542e3e95f76f11223e3c5c04e2011543be83
Sirajuddin Asjad <sirasjad@gmail.com>
2017-08-05 14:38:14 +0200

    Public release

---
+++ inc/config.php
@@ -0,0 +1,13 @@
+<?php
+// 				  Configuration file
+// ==================================================
+
+$name = 'Project Reality Roleplay';
+$version = '1.0';
+$url = 'www.pr-rp.com';
+$email = '#';
+
+$developer = 'Sira';
+$devcontact = 'sirasjad@gmail.com';
+
+?>
